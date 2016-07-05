e105d2d40842b091e484c0e9203117816f4e40a6
Benjamin Corn <bcorn@bu.edu>
2016-07-05 09:00:29 -0400

    Initial commit of binding.
    Currently intended for copy and paste usage. Future release will include separate file for binding.

---
+++ wysiwyg.js
@@ -0,0 +1,20 @@
+ko.bindingHandlers.wysivalue = {
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        // Create instance of Trumbowyg WYSIWYG on div.
+        var wysi = $(element).trumbowyg();
+        
+        // Load latest value in observable into Trumbowyg instance.
+        $(element).trumbowyg('html', ko.unwrap(valueAccessor()));
+
+    },
+
+    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+
+        // On key up, load current HTML content of Trumbowyg instance into observable.
+        var updates = $(element).keyup(function(){
+            if (ko.isWritableObservable(valueAccessor())){
+                valueAccessor()($(element).trumbowyg('html'));               
+            };
+        });
+    }
+};
